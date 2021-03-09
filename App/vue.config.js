b67bc65aa0e9ce54456e7f8212c28deb59f23ada
pashkoveval <61497581+pashkoveval@users.noreply.github.com>
2021-03-10 00:07:36 +0300

    Update vue.config.js

--- App/vue.config.js
+++ App/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     config.resolve.alias.set(
       "vue$",
       // If using the runtime only build
-      path.resolve(__dirname, "node_modules/vue/dist/vue.runtime.esm.js")
+      path.resolve(__dirname, "node_modules/vue/dist/vue.esm.js")
       // Or if using full build of Vue (runtime + compiler)
       // path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
     );
